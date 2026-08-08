c8384cf0be1ae006f74746b019c1209446edce46
Hero Studios <ahyaanthepro@gmail.com>
2026-08-08 11:57:02 +0500

    Add founder credit xHBB_ (HeroBrokenBrine) to footer/About/JSON-LD; refresh Hero Client icon

--- src/data/site.js
+++ src/data/site.js
@@ -5,6 +5,7 @@ export const site = {
   discordUrl: "https://discord.gg/5N4pAKRrkk",
   discordHandle: "herobrokenbrine",
   email: "support@herostudios.dev",
+  founder: "xHBB_ (HeroBrokenBrine)",
   issuesUrl: "/support-portal.html", // Bug reports + feature requests go through the support portal (deduped)
   donateUrl: "https://www.donationalerts.com/r/xhbb_",
   tagline: "Independent Software & Technology Studio",
